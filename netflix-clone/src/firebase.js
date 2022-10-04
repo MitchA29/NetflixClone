48dd5fa0facea50a4287ab291e26db3879fb6b63
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-10-04 12:01:50 -0500

    initial push

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBuSssieIk6BVrdeU7XHJb3c6JDI7nzVEg",
+    authDomain: "netflix-clone-76aa8.firebaseapp.com",
+    projectId: "netflix-clone-76aa8",
+    storageBucket: "netflix-clone-76aa8.appspot.com",
+    messagingSenderId: "105538358640",
+    appId: "1:105538358640:web:3f34812bbf00ac913753b5"
+  };
